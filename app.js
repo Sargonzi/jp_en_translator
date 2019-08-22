4fa5821709a830122894722c851cb21115410c8c
Zi Sar K Nar <zisarkna@isgm2.com>
2019-08-22 17:29:19 +0630

    added clear button

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-let langJP = "";
-let langEN = "";
+let langJP = document.getElementById('fromText');
+let langEN = document.getElementById('toText');
+let empty = "";
 
 document.getElementById('reset').addEventListener('click', () => {
     console.log("Reset button is clicked");
+    langJP.value = empty;
+    langEN.value = empty;
 });
